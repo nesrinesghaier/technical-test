89e505d968bd95448fd93f23eee8b1d0bcc7e899
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2022-01-27 22:35:50 +0100

    feat: add delivery entity

--- src/main/kotlin/com/gorillas/technicaltest/data/Delivery.kt
+++ src/main/kotlin/com/gorillas/technicaltest/data/Delivery.kt
@@ -1,3 +1,15 @@
 package com.gorillas.technicaltest.data
 
-data class Delivery (val id:Int,val supplier: String)
+import com.expediagroup.graphql.generator.annotations.GraphQLName
+import org.springframework.data.annotation.Id
+
+data class Delivery(
+    @GraphQLName("deliveryId")
+    @Id
+    val id: String,
+    val product: String,
+    val supplier: String,
+    val quantity: Int,
+    var expectedDate: String,
+    val expectedWarehouse: String,
+)
